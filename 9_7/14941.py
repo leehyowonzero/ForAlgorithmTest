b755e31173db41ffb011b889e1065eaf15dcee3e
leehyowon <dlgydnjs08@naver.com>
2021-09-09 13:30:31 +0000

    [Study] Algorithm

--- 9_7/14941.py
+++ 9_7/14941.py
@@ -1,31 +1,55 @@
-def prime():
-	sieve = [True for _ in range(10**5+1)]
+import bisect
+import sys
+input = sys.stdin.readline
+sieve = [True for _ in range(100001)]
 
-	m = int(10**2.5)
-	for i in range(2, m + 1):
-		if( sieve[i] == True):
-			for j in range(i+i, 10**5, i):
-				sieve[j] = False
-	
-	return [i for i in range(2, 10**5) if sieve[i] == True]
+m = int(10**2.5)
+for i in range(2, m + 1):
+	if( sieve[i] == True):
+		for j in range(i+i, 100001, i):
+			sieve[j] = False
 
-prime = prime()
-primecnt = [0 for _ in range(10**5+1)]
-for el in prime:
-	primecnt[el] += 1
-for i in range(0, 10**5):
-	primecnt[i+1] = primecnt[i+1] + primecnt[i]
-# print(primecnt)
+prime = []
+for i in range(2, 100001):
+	if sieve[i] == True:
+		prime.append(i)
+
+smeven = [0 for _ in range(100001)]
+save = 0
+for i, el in enumerate(prime):
+	if(i%2 == 0):
+		smeven[el] = save + el*3
+		save = smeven[el]
+	else:
+		smeven[el] = save - el
+		save = smeven[el]
+save = 0
+for i in range(100001):
+	if(smeven[i] == 0):
+		smeven[i] = save
+	else:
+		save = smeven[i]
+
+smodd = [0 for _ in range(100001)]
+save = 0
+for i, el in enumerate(prime):
+	if(i%2 == 0):
+		smodd[el] = save - el
+		save = smodd[el]
+	else:
+		smodd[el] = save + el*3
+		save = smodd[el]
+save = 0
+for i in range(100001):
+	if(smodd[i] == 0):
+		smodd[i] = save
+	else:
+		save = smodd[i]
 
 n = int(input())
 for _ in range(n):
-	ans = 0
 	a, b = map(int,input().split())
-	for cnt, i in enumerate(range(primecnt[a],primecnt[b]+1)):
-		if(i == 0):
-			continue
-		if(cnt % 2 == 0):
-			ans += prime[i-1] * 3 
-		else:
-			ans -= prime[i-1]
-	print(ans)
+	if(bisect.bisect_left(prime,a) %2 == 0):# even
+		print(smeven[b]- smeven[a-1])
+	else:
+		print(smodd[b]- smodd[a-1])
