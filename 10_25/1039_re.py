55e12dc4a263cbe2afc06e445ef4d0767d4d69f5
leehyowon <dlgydnjs08@naver.com>
2021-10-25 13:03:47 +0000

    [Study] Algorithm

--- 10_25/1039_re.py
+++ 10_25/1039_re.py
@@ -1,18 +1,18 @@
 from collections import deque
 
 N, K = map(int,input().split())
-LEN = len(str(N)) 
+LEN = len(N) #전체 자릿수
 answer = -1
 
 visited =[[False for i in range(11)] for i in range(1000001)]
 
 q = deque()
 q.append((N,0))
-visited[N][0] = True
+visited[N][0]=True
 
 def swap(N, idx1, idx2):
 	N = list(str(N))
-	N[idx1], N[idx2] = N[idx2], N[idx1]
+    N[idx1], N[idx2] = N[idx2], N[idx1]
 
 	return int(''.join(N))
 
@@ -25,10 +25,10 @@ while q:
        
     for i in range(LEN): 
         for j in range(i+1,LEN):
-            if i ==0 and list(str(n))[j] =='0': 
+            if i ==0 and n[j] =='0': 
                 continue
 
-            next_n = swap(n, i, j) 
+            next_n = swap(n, i, j) #자리를 바꾸고
             if not visited[next_n][depth+1]: #해당 값과 깊이를 방문하지 않았으면
                 visited[next_n][depth+1] = True #방문하고
                 q.append([next_n, depth+1]) # q에 집어넣어준다.
