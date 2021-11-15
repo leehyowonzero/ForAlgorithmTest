0625d8ef5bcc26689de4d7b48c34b57d6eecd22e
leehyowon <dlgydnjs08@naver.com>
2021-11-15 13:18:43 +0000

    [Study] Algorithm

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
-# 구글링 참고
-
-import sys
-input = sys.stdin.readline
-
-def check(x, y, k):
-	for i in range(x, x+k+1):
-		for j in range(y, y+k+1):
-			if(arr[i][j] == 0):
-				return False
-	return True
-def change(x, y, k):
-	for i in range(x, x+k+1):
-		for j in range(y, y+k+1):
-			if(arr[i][j] == 1):
-				arr[i][j] = 0
-			else:
-				arr[i][j] = 1
-
-def dfs(x, y, cnt):
-	global answer
-	if(y >= 10):
-		answer = min(answer, cnt)
-		return
-	if(x >= 10):
-		dfs(0, y+1, cnt)
-		return
-	if(arr[x][y] == 0):
-		dfs(x+1,y,cnt)
-	else: # 색종이를 붙일 수 있는 공간일 때
-		for k in range(5):
-			if(usedpaper[k] == 5):
-				continue
-			if(x + k >= 10 or y + k >= 10):
-				continue
-			if not(check(x, y, k)):
-				continue
-			else:
-				change(x, y, k)
-				usedpaper[k] += 1
-				dfs(x+k+1, y, cnt +1)
-				change(x, y, k)
-				usedpaper[k] -= 1
-
-
-
-
-arr = []
-for _ in range(10):
-	arr.append(list(map(int,input().split())))
-
-usedpaper = [0 for _ in range(5)]
-answer = sys.maxsize
-dfs(0, 0, 0)
-if(answer == sys.maxsize ):
-	print(-1)
-else:
-	print(answer)
+def NOE():
+    for i in range(n):
+        for j in range(m):
+            if Map[i][j]==1:
+                q.append((i,j))
+def Attack(q):
+    global k
+    target = [(0,Max),(0,Max),(0,Max)]
+    Dist = [Max,Max,Max]
+    while q:
+        x,y = q.popleft()
+        for i in range(3):
+            z = abs(n-x) + abs(S[i]-y)
+            if Dist[i] > z:
+                Dist[i] = z
+                target[i] = (x,y)
+            elif Dist[i] == z and y < target[i][1]:
+                target[i] = (x,y)
+    for t,(x,y) in enumerate(target):
+        if Map[x][y] == 1:
+            if Dist[t] <= d:
+                Map[x][y] = 0
+                k += 1
+				
+from itertools import combinations
+from copy import deepcopy
+from collections import deque
+Max=float('inf')
+n,m,d=map(int,input().split())
+D=[[*map(int,input().split())] for _ in range(n)]
+Castle=[i for i in range(m)]
+Archer=combinations(Castle,3)
+ans=0
+q=deque()
+for S in Archer:
+    Map=deepcopy(D)
+    k=0
+    while 1:
+        NOE()
+        if len(q)==0: break
+        Attack(q)
+        Map=[[0]*m]+Map[:n-1]
+    ans=max(ans,k)
+print(ans)
