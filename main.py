462398b33227753e9fafa37d3ed143156bc9f99c
leehyowon <dlgydnjs08@naver.com>
2022-05-11 09:23:08 +0000

    [Study] Algorithm

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-import sys
-sys.setrecursionlimit(10**6)
+from itertools import product
+n = int(input())
+m = int(input())
+wronglist = list(map(int,input().split()))
+wrongkey = [False for _ in range(10)]
+for el in wronglist:
+    wrongkey[el] = True
 
-def topdown(n, l, r):
-    global dp
-    if arr[n] == 0 :
-        return 0
- 
-    if dp[n][l][r] != None:
-        return dp[n][l][r]
- 
-    dp[n][l][r] = min(topdown(n+1, arr[n],r) + move[l][arr[n]], topdown(n+1, l, arr[n]) + move[r][arr[n]])
-    return dp[n][l][r]
-    
-move = [[1, 2, 2, 2, 2],
-       [3, 1, 3, 4, 3],
-       [4, 3, 1, 3, 4],
-       [3, 4, 3, 1, 3],
-       [4, 3, 4, 3, 1],
-       ]
-
-arr = list(map(int, sys.stdin.readline().split()))
-dp = [[[None for _ in range(5)] for _ in range(5)] for _ in range(len(arr))]
-
-answer = topdown(0, 0, 0)
-
-print(answer)
+canusekey = []
+for i in range(10):
+    if(wrongkey[i] == False):
+        canusekey.append(str(i))
+strn = str(n)
+strlen = len(strn)
+answer = abs(100 - n)
+dp = [-1 for _ in range(1000001)]
+dp[100] = 0
+for i in range(1, strlen + 1):
+    for perm in product(canusekey, repeat = i):
+        number = int(''.join(perm))
+        if(dp[number] == -1):
+            dp[number] = i
+if(dp[n] != -1):
+    print(dp[n])
+else:
+    for i in range(1, 500000):
+        candi = []
+        if(n-i >= 0):
+            if(dp[n-i] != -1):
+                candi.append(dp[n-i] + i)
+        if(n+i <= 1000000):
+            if(dp[n+i] != -1):
+                candi.append(dp[n+i] + i)
+        if candi:
+            print(min(answer, min(candi)))
+            break
+        
